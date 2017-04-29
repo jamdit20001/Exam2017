aa1d346d47ba261bc8a09c9966a96d8a1f93cf63
jamdi <jamdit20001@gmail.com>
2017-04-29 09:24:24 -0500

    Diton Exam

--- src/problems/Anagram.java
+++ src/problems/Anagram.java
@@ -1,12 +1,37 @@
 package problems;
 
+import java.util.Arrays;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class Anagram {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
+    	String str1="CAT";
+    	String str2="ACT";
+    	System.out.println("Both strings are anagrams"+ anagramCheck(str1, str2));
+    	
+    }
+    	
+    	public static boolean anagramCheck(String str1,String str2){
+    		
+    		char[] charArrayFromString1=str1.toCharArray();
+    		char[] charArrayFromString2=str2.toCharArray();
+    		Arrays.sort(charArrayFromString1);
+    		Arrays.sort(charArrayFromString2);
+    		
+    		return Arrays.equals(charArrayFromString1, charArrayFromString2);
+    		
+    		
+    		
+    		
+    		
+    		    		
+    		
+    		
+    	}
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
     }
-}
+
--- src/problems/DuplicateWord.java
+++ src/problems/DuplicateWord.java
@@ -1,4 +1,7 @@
 package problems;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by mrahman on 04/22/17.
@@ -11,8 +14,34 @@ public class DuplicateWord {
          * Also Find the average length of the words.
          */
 
-        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
-
+        String str = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        displayDuplicates(str);
     }
-
+    	public static void displayDuplicates(String str){
+    		
+    		Map<Character,Integer> map = new HashMap<>();
+    		
+    		char [] characters=str.toCharArray();
+    		
+    		for(char c : characters){
+    			
+    			if(!map.containsKey(c)){
+    				
+    				map.put(c, 1);
+    				
+    			}else{
+    				
+    				map.put(c, map.get(c)+1);
+    				
+    				}
+    			
+    			Set<Map.Entry<Character,Integer>> entrySet=map.entrySet();
+    			for(Map.Entry<Character, Integer> entry : entrySet){
+    				if(entry.getValue()>1){
+    					System.out.printf("%s : %d %n",entry.getKey(),entry.getValue());
+    				}
+    			}
+    		}
+    		
+    	}
 }
